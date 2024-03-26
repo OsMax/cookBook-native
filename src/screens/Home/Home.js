51e42451affbdd80b79228d068b3fe8840ffd317
OsMax <www.osmax@gmail.com>
2024-03-26 23:29:07 +0200

    add image background to home page

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,28 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, SafeAreaView, Button, View, Pressable } from "react-native";
+import {
+  Text,
+  SafeAreaView,
+  ImageBackground,
+  View,
+  Pressable,
+} from "react-native";
 import { styles } from "./Home.styles";
 
 export const Home = ({ navigation }) => {
   return (
-    <View>
-      <View style={styles.container}>
-        <Pressable
-          style={styles.button}
-          onPress={() => navigation.navigate("SingUp")}
-        >
-          <Text>LogUp</Text>
-        </Pressable>
-        <Pressable
-          style={styles.button}
-          onPress={() => navigation.navigate("SingIn")}
-        >
-          <Text>LogIn</Text>
-        </Pressable>
-        <Pressable style={styles.button} onPress={() => changeScreen("Posts")}>
-          <Text>Posts</Text>
-        </Pressable>
+    <ImageBackground
+      style={styles.imgBack}
+      source={require("../../../assets/images/background.webp")}
+      resizeMode="cover"
+    >
+      <View>
+        <View style={styles.container}>
+          <Pressable
+            style={styles.button}
+            onPress={() => navigation.navigate("SingUp")}
+          >
+            <Text>LogUp</Text>
+          </Pressable>
+          <Pressable
+            style={styles.button}
+            onPress={() => navigation.navigate("SingIn")}
+          >
+            <Text>LogIn</Text>
+          </Pressable>
+          <Pressable
+            style={styles.button}
+            onPress={() => changeScreen("Posts")}
+          >
+            <Text>Posts</Text>
+          </Pressable>
+        </View>
+        <StatusBar style="auto" />
       </View>
-      <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 };
