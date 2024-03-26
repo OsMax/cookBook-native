df66d44fca1b07e6e9a73fa26d7757ad2596eb65
OsMax <www.osmax@gmail.com>
2024-03-26 23:07:20 +0200

    create project

---
+++ App.js
@@ -0,0 +1,49 @@
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import { RegistrationScreen } from "./src/screens/RegistrationScreen/RegistrationScreen";
+import { LoginScreen } from "./src/screens/LoginScreen/LoginScreen";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto/Roboto-Black.ttf"),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="SingUp"
+              component={RegistrationScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="SingIn"
+              component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <StatusBar style="auto" />
+      </>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "Roboto-Black",
+    fontSize: 30,
+  },
+});
