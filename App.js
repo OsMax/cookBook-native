9e67dae40c2d3367ca0fd8e85da2d5648e5aaee3
OsMax <www.osmax@gmail.com>
2024-03-26 23:27:20 +0200

    Home page to first screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import { useFonts } from "expo-font";
+import { Home } from "./src/screens/Home/Home";
 import { RegistrationScreen } from "./src/screens/RegistrationScreen/RegistrationScreen";
 import { LoginScreen } from "./src/screens/LoginScreen/LoginScreen";
 
@@ -23,6 +24,11 @@ export default function App() {
       <>
         <NavigationContainer>
           <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ headerShown: false }}
+            />
             <Stack.Screen
               name="SingUp"
               component={RegistrationScreen}
