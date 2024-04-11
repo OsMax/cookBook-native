95deee2e249f65de1391c1679884e483e8b0d707
OsMax <www.osmax@gmail.com>
2024-04-11 15:54:21 +0300

    add footer container when user is login

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { styles } from "./Footer.styles";
+
+export const Footer = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>FOOTER</Text>
+    </View>
+  );
+};
---
+++ src/components/Footer/Footer.styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: "center",
+    width: "100%",
+    height: 100,
+    position: "absolute",
+    bottom: 0,
+    alignItems: "center",
+    borderColor: "#000",
+  },
+  text: {
+    color: "#fff",
+  },
+});
