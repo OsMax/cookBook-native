fa23fc853475eec63a0fbc37325a5d617927e599
OsMax <www.osmax@gmail.com>
2024-04-15 16:41:42 +0300

    loader in selector

--- src/redux/auth/authSelector.js
+++ src/redux/auth/authSelector.js
@@ -1,5 +1,6 @@
 // import { createSelector } from '@reduxjs/toolkit';
 
-export const selectUser = state => state.auth.user;
-export const selectToken = state => state.auth.token;
-export const selectIsLogIn = state => state.auth.isLogIn;
+export const selectUser = (state) => state.auth.user;
+export const selectToken = (state) => state.auth.token;
+export const selectIsLogIn = (state) => state.auth.isLogIn;
+export const selectLoader = (state) => state.auth.loader;
