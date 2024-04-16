5490123af1dc7045b58b73d11a0f52fa502dc069
OsMax <www.osmax@gmail.com>
2024-04-16 15:33:38 +0300

    add loader to home screen for current user

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -8,32 +8,44 @@ import {
 } from "react-native";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { selectUser } from "../../redux/auth/authSelector";
+import {
+  selectUser,
+  selectIsLogIn,
+  selectLoader,
+} from "../../redux/auth/authSelector";
 import { styles } from "./Home.styles";
 import { Header } from "../../components/Header/Header";
 import { Recipes } from "../../components/Recipes/Recipes";
+import { Footer } from "../../components/Footer/Footer";
+import { Loader } from "../../components/Loader/Loader";
 
 export const Home = ({ navigation }) => {
-  const [isLogIn, setIsLogIn] = useState(false);
   const user = useSelector(selectUser);
+  const isLogin = useSelector(selectIsLogIn);
+  const isLoade = useSelector(selectLoader);
+
   useEffect(() => {
-    if (user.name) {
+    if (isLogin) {
       // navigation.navigate("Recipes");
       console.log("11111");
     }
-  }, [user]);
+  }, [user, isLogin]);
   return (
-    <ImageBackground
-      style={styles.imgBack}
-      source={require("../../../assets/images/background.webp")}
-      resizeMode="cover"
-    >
-      {/* <View> */}
-      <Header navigation={navigation} />
-      <Recipes />
+    <>
+      <ImageBackground
+        style={styles.imgBack}
+        source={require("../../../assets/images/background.webp")}
+        resizeMode="cover"
+      >
+        {/* <View> */}
+        <Header navigation={navigation} />
+        <Recipes />
+        {isLogin && <Footer />}
 
-      <StatusBar style="auto" />
-      {/* </View> */}
-    </ImageBackground>
+        <StatusBar style="auto" />
+        {/* </View> */}
+      </ImageBackground>
+      {isLoade && <Loader />}
+    </>
   );
 };
