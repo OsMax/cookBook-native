1dbea821f26a4e08435d4cc6aa667a84a79d70d1
OsMax <www.osmax@gmail.com>
2024-04-21 21:39:25 +0300

    fix in registration slice

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -11,43 +11,40 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  // extraReducers: {
-  //   [register.fulfilled](state, action) {
-  //     state.user = action.payload.user;
-  //     state.token = action.payload.token;
-  //     state.isLogIn = true;
-  //   },
-  //   [login.fulfilled](state, action) {
-  //     state.user = action.payload.user;
-  //     state.token = action.payload.token;
-  //     state.isLogIn = true;
-  //   },
-  //   [logOut.fulfilled](state) {
-  //     state.user = { name: '', email: '' };
-  //     state.token = null;
-  //     state.isLogIn = false;
-  //   },
-  // },
   extraReducers: (builder) => {
     builder
+      // register
+      .addCase(register.pending, (state) => {
+        state.loader = true;
+      })
       .addCase(register.fulfilled, (state, { payload }) => {
         state.user = { ...payload.user };
         state.isLogIn = true;
-        // console.log(payload.user);
         state.token = payload.token;
+        state.loader = false;
+      })
+      .addCase(register.rejected, (state) => {
+        state.isLogIn = false;
+        state.loader = false;
+      })
+      // current
+      .addCase(current.pending, (state) => {
+        state.loader = true;
       })
       .addCase(current.fulfilled, (state, { payload }) => {
         if (payload.user) {
           state.user = { ...payload.user };
           state.isLogIn = true;
+          state.loader = false;
         }
-        // state.user = { ...payload.user };
       })
       .addCase(current.rejected, (state) => {
         state.token = null;
         state.user = { ...initialState.user };
         state.isLogIn = false;
+        state.loader = false;
       })
+      // login
       .addCase(logIn.pending, (state) => {
         state.loader = true;
       })
