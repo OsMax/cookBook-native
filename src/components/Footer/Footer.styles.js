6b8346a6eff0688a7ec18cde63462d28d7a2342a
OsMax <www.osmax@gmail.com>
2024-04-21 21:35:04 +0300

    styles for footer

--- src/components/Footer/Footer.styles.js
+++ src/components/Footer/Footer.styles.js
@@ -1,11 +1,11 @@
 import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 0,
     // justifyContent: "center",
     width: "100%",
-    height: 100,
-    position: "absolute",
+    height: 64,
+    // position: "absolute",
     bottom: 0,
     alignItems: "center",
     borderColor: "#000",
