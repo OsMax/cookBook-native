8b867226d754028c23f22cbcbbff0863787879cb
OsMax <www.osmax@gmail.com>
2024-04-21 21:35:59 +0300

    header: header  for login and unlogin user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,42 @@
-import { View, Pressable, Text } from "react-native";
+import { View, Pressable, Text, Image } from "react-native";
+import { selectUser, selectIsLogIn } from "../../redux/auth/authSelector";
+import { useSelector } from "react-redux";
 import { styles } from "./Header.styles";
+import { useEffect, useState } from "react";
+import { UserMenu } from "../UserMenu/UserMenu";
 
 export const Header = ({ navigation }) => {
+  const user = useSelector(selectUser);
+  const isLogin = useSelector(selectIsLogIn);
+  const [showMenu, setShowMenu] = useState(false);
+  useEffect(() => {
+    if (!isLogin) {
+      setShowMenu(false);
+    }
+  }, [isLogin]);
   return (
-    <View style={styles.container}>
-      {/* <Pressable
-        style={styles.button}
-        onPress={() => navigation.navigate("SingUp")}
-      >
-        <Text style={styles.btnText}>LogUp</Text>
-      </Pressable> */}
-      <Pressable
-        style={styles.button}
-        onPress={() => navigation.navigate("SingIn")}
-      >
-        <Text style={styles.btnText}>LogIn</Text>
-      </Pressable>
+    // <View style={[styles.container, { height: 20 }]}>
+    <View style={[styles.container]}>
+      {!isLogin && (
+        <Pressable
+          style={styles.button}
+          onPress={() => navigation.navigate("SingIn")}
+        >
+          <Text style={styles.btnText}>LogIn</Text>
+        </Pressable>
+      )}
+      {isLogin && (
+        <>
+          <Pressable style={styles.button} onPress={() => setShowMenu(true)}>
+            <Text style={styles.btnText}>{user.name}</Text>
+            <Image
+              style={{ width: 40, height: 40, borderRadius: 20 }}
+              source={{ uri: user.avatarURL }}
+            />
+          </Pressable>
+          <UserMenu user={user} showMenu={showMenu} setShowMenu={setShowMenu} />
+        </>
+      )}
     </View>
   );
 };
