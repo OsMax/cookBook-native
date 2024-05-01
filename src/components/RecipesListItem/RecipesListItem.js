bd32ceb19d483562018834cd1f8c4fcc526829b8
OsMax <www.osmax@gmail.com>
2024-05-01 16:08:34 +0300

    create new component for recipe item

---
+++ src/components/RecipesListItem/RecipesListItem.js
@@ -0,0 +1,99 @@
+import { Text, View, Image, Dimensions, Pressable } from "react-native";
+
+export const recipesItem = ({ item, setReadRecipe }) => {
+  const date = item.date.split("T")[0];
+  const widthIteminfo = Dimensions.get("window").width * 0.95 - 158;
+
+  return (
+    <>
+      <Pressable
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "flex-start",
+          width: "100%",
+          borderWidth: 2,
+          borderRadius: 20,
+          borderColor: "#FF6C00",
+          marginTop: 10,
+          padding: 4,
+          backgroundColor: "#ffefef",
+        }}
+        onPress={() => setReadRecipe(item)}
+      >
+        <Image
+          style={{ width: 150, height: 140, borderRadius: 14 }}
+          source={{ uri: item.imageUrl }}
+        />
+        <View
+          style={{
+            // flexGrow: 1,
+            width: widthIteminfo,
+            padding: 4,
+            height: "100%",
+            // alignItems: "center",
+            // flexDirection: "row",
+          }}
+        >
+          <Text
+            style={{
+              color: "#FF6C00",
+              fontSize: 20,
+              textShadowColor: "#999", // Цвет тени
+              textShadowOffset: { width: 1, height: 1 }, // Смещение тени
+              textShadowRadius: 2,
+              textAlign: "center",
+            }}
+          >
+            {item.name}
+          </Text>
+          <View
+            style={{
+              width: "100%",
+              maxHeight: "100%",
+              flexGrow: 1,
+              justifyContent: "space-between",
+            }}
+          >
+            <View
+              style={{
+                // width: "100%",
+                padding: 8,
+                flexDirection: "row",
+                flexWrap: "wrap",
+                rowGap: 4,
+                columnGap: 6,
+                alignItems: "flex-start",
+                justifyContent: "center",
+              }}
+            >
+              {item.ingredients.map((ing) => (
+                <Text
+                  key={item._id + ing}
+                  style={{
+                    backgroundColor: "#FF6C00",
+                    borderRadius: 8,
+                    paddingHorizontal: 8,
+                    paddingVertical: 2,
+                    color: "#fff",
+                  }}
+                >
+                  {ing}
+                </Text>
+              ))}
+            </View>
+            <View
+              style={{
+                width: "100%",
+                alignItems: "flex-end",
+                paddingRight: 4,
+              }}
+            >
+              <Text>{date}</Text>
+            </View>
+          </View>
+        </View>
+      </Pressable>
+    </>
+  );
+};
