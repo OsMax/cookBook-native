a5528421b81cd74376cd5d011b84863adaa31189
OsMax <www.osmax@gmail.com>
2024-05-23 15:22:30 +0300

    operation for dd recipe

---
+++ src/redux/comments/comOperation.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+// const BASEURL = "http://192.168.88.16:3000"; //home
+const BASEURL = "http://192.168.226.123:3000"; //work
+// const BASEURL = "https://cookbook-t2ch.onrender.com";
+
+axios.defaults.baseURL = BASEURL;
+const tokenSet = (token) => {
+  axios.defaults.headers.common.authorization = token;
+};
+
+export const addComment = createAsyncThunk(
+  "comment/add",
+  async (data, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const { id, text } = data;
+    if (!state.auth.token) return thunkAPI.rejectWithValue("Without token");
+    tokenSet(`Bearer ${state.auth.token}`);
+
+    const comment = { text };
+    comment.date = new Date();
+
+    try {
+      const result = await axios.post(`/api/comments/${id}`);
+      //   return result;
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
