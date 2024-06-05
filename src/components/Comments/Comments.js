a7d2aa6795e798c793e0b4e08c5a906c081a18a8
OsMax <www.osmax@gmail.com>
2024-06-05 15:14:28 +0300

    Create Comments.js

---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,21 @@
+import {
+  View,
+  Pressable,
+  Text,
+  Image,
+  Modal,
+  // KeyboardAvoidingView,
+  // Keyboard,
+  // Platform,
+  TouchableWithoutFeedback,
+  TextInput,
+  ScrollView,
+} from "react-native";
+
+export const Comments = (recipeId) => {
+  return (
+    <View>
+      <Text>Comments will be here</Text>
+    </View>
+  );
+};
