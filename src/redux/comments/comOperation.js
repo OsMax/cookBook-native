d27be457c66cab24d037393c62d70f37bee2677b
OsMax <www.osmax@gmail.com>
2024-06-11 15:03:45 +0300

    operation for add comment to db

--- src/redux/comments/comOperation.js
+++ src/redux/comments/comOperation.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 // const BASEURL = "http://192.168.88.16:3000"; //home
-const BASEURL = "http://192.168.226.123:3000"; //work
+const BASEURL = "http://192.168.241.123:3000"; //work
 // const BASEURL = "https://cookbook-t2ch.onrender.com";
 
 axios.defaults.baseURL = BASEURL;
@@ -15,21 +15,26 @@ const tokenSet = (token) => {
 export const addComment = createAsyncThunk(
   "comment/add",
   async (data, thunkAPI) => {
+    // console.log(data);
+    // return data;
     const state = thunkAPI.getState();
-    const { id, text } = data;
-    if (!state.auth.token) return thunkAPI.rejectWithValue("Without token");
+    const { recipeId, comment } = data;
+    // if (!state.auth.token) return thunkAPI.rejectWithValue("Without token");
     tokenSet(`Bearer ${state.auth.token}`);
+    // console.log(comment);
 
-    const comment = { text };
-    comment.date = new Date();
+    // const comment = { text };
+    // comment.date = new Date();
+    const myComment = { comment, date: new Date() };
 
     try {
-      const result = await axios.post(`/api/comments/${id}`, comment);
+      const result = await axios.post(`/api/comments/${recipeId}`, myComment);
       //   return result;
       console.log(result);
     } catch (error) {
       console.log(error);
     }
+    // console.log(data);
   }
 );
 
