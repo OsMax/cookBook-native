6481385907525ae5704485ae4f6818ca3a9310bb
OsMax <www.osmax@gmail.com>
2024-06-19 13:57:03 +0300

    simple comment list

--- src/components/CommentsList/CommentsList.js
+++ src/components/CommentsList/CommentsList.js
@@ -1,1 +1,36 @@
-export const CommentsList = (comments) => {};
+import { View, Text, FlatList, Image } from "react-native";
+
+export const CommentsList = ({ comments }) => {
+  return (
+    <View style={{ width: "100%" }}>
+      {comments.map((comment) => (
+        <View
+          key={comment._id}
+          style={{ width: "100%", display: "flex", flexDirection: "row" }}
+        >
+          <View>
+            <Image
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+              }}
+              source={{ uri: comment.avatarURL }}
+            />
+            <Text>{comment.ownerName}</Text>
+          </View>
+          <View>
+            <Text>{comment.commentText}</Text>
+            <Text>
+              {comment.date.split("T")[0] +
+                " " +
+                comment.date.split("T")[1].split(":")[0] +
+                ":" +
+                comment.date.split("T")[1].split(":")[1]}
+            </Text>
+          </View>
+        </View>
+      ))}
+    </View>
+  );
+};
